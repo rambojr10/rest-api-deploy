c50877700356876e9593c3508a250d16ae235465
rambojr10 <valenciapipe1@gmail.com>
2024-08-12 13:32:27 -0500

    Deploy API to Vercel

---
+++ schemas/movies.js
@@ -0,0 +1,37 @@
+const z = require('zod')
+
+const currentYear = new Date().getFullYear()
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie title must be a string',
+    required_error: 'Movie title is required'
+  }),
+  year: z.number().int().min(1900).max(currentYear),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  poster: z.string().url({
+    message: 'Invalid URL format for poster'
+  }),
+  genre: z.array(
+    z.enum(['Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
+    {
+      required_error: 'Movie genre is required',
+      invalid_type_error: 'Movie genre must be an array of enum Genre'
+    }
+  ),
+  rate: z.number().min(0).max(10).default(5)
+})
+
+function validateMovie (object) {
+  return movieSchema.safeParse(object)
+}
+
+function validatePartialMovie (object) {
+  return movieSchema.partial().safeParse(object)
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+}
