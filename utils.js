75883173c19c9a25b18df5ca663b065b5c3254eb
rambojr10 <valenciapipe1@gmail.com>
2024-09-18 23:36:44 -0500

    Updating utils.js file

--- utils.js
+++ utils.js
@@ -3,12 +3,16 @@
 // const require = createRequire(import.meta.url)
 // export const readJson = path => require(path)
 
-import { promises as fs } from 'node:fs'
+import { promises as fs } from 'fs'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export const readJson = async (relativePath) => {
   try {
-    const filePath = path.resolve(relativePath)
+    const filePath = path.join(__dirname, relativePath)
     const data = await fs.readFile(filePath, 'utf-8')
     return JSON.parse(data)
   } catch (err) {
